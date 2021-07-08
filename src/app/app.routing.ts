734447498fc46ae675e37c446b958eb45c4ca689
Jyoti Vankala <jyotivankala123@gmail.com>
2021-07-08 06:57:12 +0530

    a

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,7 @@ const routes: Routes = [{
     data: {
       title: 'Login Page'
     },
-  },              
+  },                     
   {
     path: 'register',
     component: RegisterComponent,
