cc6469f713402321afc715743231fb6494dbe9c2
Jyoti Vankala <jyotivankala123@gmail.com>
2021-07-18 18:24:12 +0530

    upload

--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'pages-dashboard',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+ constructor(private router:Router) { 
 
+		let access_token = localStorage.getItem('access-token');
+		console.log(access_token);
+	}
   ngOnInit(): void {
   }
+  logout(){
+		localStorage.removeItem('access-token');
+        localStorage.removeItem('refresh-token');
+        localStorage.removeItem('is_active');
+        this.router.navigate(['/login']); 
+	}
+
 
 }
